85da9f9639983ed897fef8563c9ad09074e34c2d
sanya238 <35998640+sanya238@users.noreply.github.com>
2018-02-13 06:06:10 +0300

    Add files via upload

---
+++ client.py
@@ -0,0 +1,56 @@
+﻿import socket
+
+class Client:
+    def __init__(self,host,port, timeout=None):
+        self.host=host
+        self.port=port
+        self.timeout=timeout
+        self.sock=socket.create_connection((self.host,self.port),self.timeout)
+#Записываем информацию на сервер
+    def put(self, key,value,timestamp):
+
+        self.stringput="put "+key+" "+str(value)+" "+str(timestamp)+"\n"
+        self.sock.sendall(self.stringput.encode("utf8"))
+        self.stringget = self.sock.recv(1024)
+        self.stringget = self.stringget.decode("utf8")
+
+#Получаем информацию с сервера
+    def get(self,key):
+        self.stringput="get"+" "+key+"\n"
+        self.sock.sendall(self.stringput.encode("utf8"))
+
+        self.stringget=self.sock.recv(1024)
+        self.stringget=self.stringget.decode("utf8")
+        self.result=self.stringget.splitlines()
+        self.return_dict=dict()
+        if self.stringget=="ok\n\n":
+            return  self.return_dict
+        elif self.result[0]=="ok":
+
+            for line in self.result:
+                if line == "ok":
+                    continue
+                temp=line.split()
+                if len(temp)==3:
+                    temp_k= (int (temp[2]),float(temp[1]))
+                    if temp[0] in self.return_dict:
+                        self.return_dict[temp[0]].append(temp_k)
+                    else:
+                        temp_list=[temp_k]
+                        self.return_dict[temp[0]]=temp_list
+
+
+
+            return self.return_dict
+
+    def __del(self):
+        self.sock.close()
+
+class ClientError(Exception):
+    def __init__(self,value):
+        self.msg=value
+    def __str__(self):
+        return self.msg
+
+
+
---
+++ server.py
@@ -0,0 +1,86 @@
+﻿import asyncio
+
+#Словарь для хранения метрик
+database=dict()
+#Обработка команды put
+def put (stroka):
+    if len(stroka)==4:
+        temp_k=[stroka[2],stroka[3]]
+        if stroka[1] in database:
+            database[stroka[1]].append(temp_k)
+        else:
+            temp_list=[temp_k]
+            database[stroka[1]]=temp_list
+        respond = "ok\n\n"
+    else:
+        respond = "error\nwrong command\n\n"
+    return respond
+#Обработка команды get
+def get (stroka):
+    print (stroka[1],len(stroka))
+    if len(stroka) == 2:
+        respond="ok\n"
+        if stroka[1]=="*":
+            for key in database:
+                temp_list= database[key]
+                for keyin in temp_list:
+                    val,timestamp = keyin
+                    respond = respond + str(key)+" "+ str(val)+" "+str(timestamp)+"\n"
+        else:
+            if stroka[1] in database:
+                temp_list= database[stroka[1]]
+                for keyin in temp_list:
+                    val,timestamp = keyin
+                    respond = respond + str(stroka[1])+" "+ str(val)+" "+str(timestamp)+"\n"
+            else:
+                respond = "ok\n"
+        respond+="\n"
+    else:
+        respond = "error\nwrong command\n\n"
+    return respond
+
+#запуск корутины при подключении. Так как в одном сеансе возможно несколько команд,соединение разрывается по инициативе клиента
+async def handle_echo(reader, writer):
+   try:
+        while not reader.at_eof():
+            data = await reader.read(1024)
+            message = data.decode()
+            com_str=message.replace('\n','').split()
+            print(message)
+#Проверка команды
+            if len(com_str)>0 and com_str[0]=="put":
+                resp=put(com_str)
+            elif len(com_str)>0 and com_str[0]=="get":
+                resp=get(com_str)
+            else:
+                resp="error\nwrong command\n\n"
+            writer.write(resp.encode())
+            print(resp)
+            print (database)
+   finally:
+        writer.close()
+#Запуск сервера
+
+def run_server(host,port):
+    print (host,port)
+    port=int(port)
+    loop = asyncio.get_event_loop()
+    coro = asyncio.start_server(
+        handle_echo,
+        host, port, loop=loop
+    )
+
+    server = loop.run_until_complete(coro)
+
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
+
+    server.close()
+    loop.run_until_complete(server.wait_closed())
+    loop.close()
+
+#Для теста
+if  __name__ ==  "__main__" :
+    run_server("127.0.0.1",8881)
